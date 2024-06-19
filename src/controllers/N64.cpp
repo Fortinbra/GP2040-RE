dae2adf5bf5529bbef5f4d87c25ad4ffbc61eed5
Fortinbra <fortinbra@becauseimclever.com>
2024-06-18 19:24:12 -0500

    Cleanup

--- src/controllers/N64.cpp
+++ src/controllers/N64.cpp
@@ -1,234 +1,234 @@
-#include "headers/N64.h"
-#include <stdio.h>
-#include "pico/stdlib.h"
+// #include "headers/N64.h"
+// #include <stdio.h>
+// #include "pico/stdlib.h"
 
-N64::N64(int dataPin)
-	: dataPin(dataPin)
-{
-	gpio_init(dataPin);
-	gpio_set_dir(dataPin, GPIO_IN);
-}
+// N64::N64(int dataPin)
+// 	: dataPin(dataPin)
+// {
+// 	gpio_init(dataPin);
+// 	gpio_set_dir(dataPin, GPIO_IN);
+// }
 
-uint16_t N64::translateToFormat(GamepadState data)
-{
-	uint16_t n64Data = 0;
+// uint16_t N64::translateToFormat(GamepadState data)
+// {
+// 	uint16_t n64Data = 0;
 
-	// Map each button in the GamepadState to the corresponding bit in the N64 format
-	if (data.buttons & GAMEPAD_MASK_B1)
-	{
-		n64Data |= N64_BUTTON_A;
-	}
-	if (data.buttons & GAMEPAD_MASK_B2)
-	{
-		n64Data |= N64_BUTTON_B;
-	}
-	if (data.buttons & GAMEPAD_MASK_B3)
-	{
-		n64Data |= N64_BUTTON_Z;
-	}
-	if (data.buttons & GAMEPAD_MASK_L1)
-	{
-		n64Data |= N64_BUTTON_L;
-	}
-	if (data.buttons & GAMEPAD_MASK_R1)
-	{
-		n64Data |= N64_BUTTON_R;
-	}
-	if (data.buttons & GAMEPAD_MASK_S1)
-	{
-		n64Data |= N64_BUTTON_START;
-	}
-	if (data.dpad & GAMEPAD_MASK_DOWN)
-	{
-		n64Data |= N64_BUTTON_DPAD_DOWN;
-	}
-	if (data.dpad & GAMEPAD_MASK_UP)
-	{
-		n64Data |= N64_BUTTON_DPAD_UP;
-	}
-	if (data.dpad & GAMEPAD_MASK_RIGHT)
-	{
-		n64Data |= N64_BUTTON_DPAD_RIGHT;
-	}
-	if (data.dpad & GAMEPAD_MASK_LEFT)
-	{
-		n64Data |= N64_BUTTON_DPAD_LEFT;
-	}
+// 	// Map each button in the GamepadState to the corresponding bit in the N64 format
+// 	if (data.buttons & GAMEPAD_MASK_B1)
+// 	{
+// 		n64Data |= N64_BUTTON_A;
+// 	}
+// 	if (data.buttons & GAMEPAD_MASK_B2)
+// 	{
+// 		n64Data |= N64_BUTTON_B;
+// 	}
+// 	if (data.buttons & GAMEPAD_MASK_B3)
+// 	{
+// 		n64Data |= N64_BUTTON_Z;
+// 	}
+// 	if (data.buttons & GAMEPAD_MASK_L1)
+// 	{
+// 		n64Data |= N64_BUTTON_L;
+// 	}
+// 	if (data.buttons & GAMEPAD_MASK_R1)
+// 	{
+// 		n64Data |= N64_BUTTON_R;
+// 	}
+// 	if (data.buttons & GAMEPAD_MASK_S1)
+// 	{
+// 		n64Data |= N64_BUTTON_START;
+// 	}
+// 	if (data.dpad & GAMEPAD_MASK_DOWN)
+// 	{
+// 		n64Data |= N64_BUTTON_DPAD_DOWN;
+// 	}
+// 	if (data.dpad & GAMEPAD_MASK_UP)
+// 	{
+// 		n64Data |= N64_BUTTON_DPAD_UP;
+// 	}
+// 	if (data.dpad & GAMEPAD_MASK_RIGHT)
+// 	{
+// 		n64Data |= N64_BUTTON_DPAD_RIGHT;
+// 	}
+// 	if (data.dpad & GAMEPAD_MASK_LEFT)
+// 	{
+// 		n64Data |= N64_BUTTON_DPAD_LEFT;
+// 	}
 
-	// Add the joystick data
-	n64Data <<= 8;										   // Shift the button data left by 8 bits to make room for the joystick data
-	n64Data |= reverseBits(scaleJoystick(data.lx) & 0xFF); // Add the X-axis joystick data
-	n64Data <<= 8;										   // Shift the data left by 8 bits again to make room for more joystick data
-	n64Data |= reverseBits(scaleJoystick(data.ly) & 0xFF); // Add the Y-axis joystick data
-	N64ControllerState = n64Data;
-	return n64Data;
-}
-int N64::scaleJoystick(int gamepadValue)
-{
-	// Subtract the midpoint of the gamepad range to center the value around 0
-	int centeredValue = gamepadValue - GAMEPAD_JOYSTICK_MID;
+// 	// Add the joystick data
+// 	n64Data <<= 8;										   // Shift the button data left by 8 bits to make room for the joystick data
+// 	n64Data |= reverseBits(scaleJoystick(data.lx) & 0xFF); // Add the X-axis joystick data
+// 	n64Data <<= 8;										   // Shift the data left by 8 bits again to make room for more joystick data
+// 	n64Data |= reverseBits(scaleJoystick(data.ly) & 0xFF); // Add the Y-axis joystick data
+// 	N64ControllerState = n64Data;
+// 	return n64Data;
+// }
+// int N64::scaleJoystick(int gamepadValue)
+// {
+// 	// Subtract the midpoint of the gamepad range to center the value around 0
+// 	int centeredValue = gamepadValue - GAMEPAD_JOYSTICK_MID;
 
-	// Scale the centered value to the N64 range
-	int n64Value = centeredValue * 80 / GAMEPAD_JOYSTICK_MID;
+// 	// Scale the centered value to the N64 range
+// 	int n64Value = centeredValue * 80 / GAMEPAD_JOYSTICK_MID;
 
-	// Clamp the value to the N64 range
-	if (n64Value < -80)
-	{
-		n64Value = -80;
-	}
-	else if (n64Value > 80)
-	{
-		n64Value = 80;
-	}
+// 	// Clamp the value to the N64 range
+// 	if (n64Value < -80)
+// 	{
+// 		n64Value = -80;
+// 	}
+// 	else if (n64Value > 80)
+// 	{
+// 		n64Value = 80;
+// 	}
 
-	return n64Value;
-}
-uint8_t N64::reverseBits(uint8_t num)
-{
-	uint8_t NO_OF_BITS = sizeof(num) * 8;
-	uint8_t reverse_num = 0;
-	int i;
-	for (i = 0; i < NO_OF_BITS; i++)
-	{
-		if ((num & (1 << i)))
-			reverse_num |= 1 << ((NO_OF_BITS - 1) - i);
-	}
-	return reverse_num;
-}
-void N64::listenForCommands()
-{
-	// Set the data pin to input mode to listen for commands
-	gpio_set_dir(dataPin, GPIO_IN);
+// 	return n64Value;
+// }
+// uint8_t N64::reverseBits(uint8_t num)
+// {
+// 	uint8_t NO_OF_BITS = sizeof(num) * 8;
+// 	uint8_t reverse_num = 0;
+// 	int i;
+// 	for (i = 0; i < NO_OF_BITS; i++)
+// 	{
+// 		if ((num & (1 << i)))
+// 			reverse_num |= 1 << ((NO_OF_BITS - 1) - i);
+// 	}
+// 	return reverse_num;
+// }
+// void N64::listenForCommands()
+// {
+// 	// Set the data pin to input mode to listen for commands
+// 	gpio_set_dir(dataPin, GPIO_IN);
 
-	while (true) // Keep listening for commands indefinitely
-	{
-		// Wait for the start bit
-		while (gpio_get(dataPin) == 1)
-		{
-		}
+// 	while (true) // Keep listening for commands indefinitely
+// 	{
+// 		// Wait for the start bit
+// 		while (gpio_get(dataPin) == 1)
+// 		{
+// 		}
 
-		// Wait for the line to go high again
-		sleep_us(2);
-		if (gpio_get(dataPin) == 0)
-			continue;
-		// Initialize an empty buffer to store the command and data bytes
-		std::vector<uint8_t> buffer;
+// 		// Wait for the line to go high again
+// 		sleep_us(2);
+// 		if (gpio_get(dataPin) == 0)
+// 			continue;
+// 		// Initialize an empty buffer to store the command and data bytes
+// 		std::vector<uint8_t> buffer;
 
-		// Read the command byte
-		uint8_t command = readByte();
+// 		// Read the command byte
+// 		uint8_t command = readByte();
 
-		// Store the command byte in the buffer
-		buffer.push_back(command);
+// 		// Store the command byte in the buffer
+// 		buffer.push_back(command);
 
-		// Continue reading and storing bytes in the buffer until the STOP bit is encountered
-		while (gpio_get(dataPin) != 1)
-		{
-			uint8_t data = readByte();
-			buffer.push_back(data);
-		}
+// 		// Continue reading and storing bytes in the buffer until the STOP bit is encountered
+// 		while (gpio_get(dataPin) != 1)
+// 		{
+// 			uint8_t data = readByte();
+// 			buffer.push_back(data);
+// 		}
 
-		// Respond to the command
-		switch (command)
-		{
-		case N64Command::RESETANDINFO:
-			resetController();
-			sendControllerInfo();
-			break;
-		case N64Command::INFO:
-			sendControllerInfo();
-			break;
-		case N64Command::CONTROLLER_STATE:
-			sendControllerState();
-			break;
-		case N64Command::READ_CONTROLLER_ACCESSORY:
-			// handle READ_CONTROLLER_ACCESSORY command
-			break;
-		case N64Command::WRITE_CONTROLLER_ACCESSORY:
-			// handle WRITE_CONTROLLER_ACCESSORY command
-			break;
-		case N64Command::READ_EEPROM:
-			// handle READ_EEPROM command
-			break;
-		case N64Command::WRITE_EEPROM:
-			// handle WRITE_EEPROM command
-			break;
-		case N64Command::RTC_INFO:
-			// handle RTC_INFO command
-			break;
-		case N64Command::READ_RTC_BLOCK:
-			// handle READ_RTC_BLOCK command
-			break;
-		case N64Command::WRITE_RTC_BLOCK:
-			// handle WRITE_RTC_BLOCK command
-			break;
-		}
-	}
-}
-uint8_t N64::readByte()
-{
-	uint8_t value = 0;
-	for (int i = 0; i < 8; i++)
-	{
-		// Measure the duration of the low period
-		auto start = std::chrono::high_resolution_clock::now();
-		while (gpio_get(dataPin) == 0)
-		{
-		}
-		auto end = std::chrono::high_resolution_clock::now();
-		auto low_duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+// 		// Respond to the command
+// 		switch (command)
+// 		{
+// 		case N64Command::RESETANDINFO:
+// 			resetController();
+// 			sendControllerInfo();
+// 			break;
+// 		case N64Command::INFO:
+// 			sendControllerInfo();
+// 			break;
+// 		case N64Command::CONTROLLER_STATE:
+// 			sendControllerState();
+// 			break;
+// 		case N64Command::READ_CONTROLLER_ACCESSORY:
+// 			// handle READ_CONTROLLER_ACCESSORY command
+// 			break;
+// 		case N64Command::WRITE_CONTROLLER_ACCESSORY:
+// 			// handle WRITE_CONTROLLER_ACCESSORY command
+// 			break;
+// 		case N64Command::READ_EEPROM:
+// 			// handle READ_EEPROM command
+// 			break;
+// 		case N64Command::WRITE_EEPROM:
+// 			// handle WRITE_EEPROM command
+// 			break;
+// 		case N64Command::RTC_INFO:
+// 			// handle RTC_INFO command
+// 			break;
+// 		case N64Command::READ_RTC_BLOCK:
+// 			// handle READ_RTC_BLOCK command
+// 			break;
+// 		case N64Command::WRITE_RTC_BLOCK:
+// 			// handle WRITE_RTC_BLOCK command
+// 			break;
+// 		}
+// 	}
+// }
+// uint8_t N64::readByte()
+// {
+// 	uint8_t value = 0;
+// 	for (int i = 0; i < 8; i++)
+// 	{
+// 		// Measure the duration of the low period
+// 		auto start = std::chrono::high_resolution_clock::now();
+// 		while (gpio_get(dataPin) == 0)
+// 		{
+// 		}
+// 		auto end = std::chrono::high_resolution_clock::now();
+// 		auto low_duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 
-		// Measure the duration of the high period
-		start = std::chrono::high_resolution_clock::now();
-		while (gpio_get(dataPin) == 1)
-		{
-		}
-		end = std::chrono::high_resolution_clock::now();
-		auto high_duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+// 		// Measure the duration of the high period
+// 		start = std::chrono::high_resolution_clock::now();
+// 		while (gpio_get(dataPin) == 1)
+// 		{
+// 		}
+// 		end = std::chrono::high_resolution_clock::now();
+// 		auto high_duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 
-		// Check the durations to determine the bit value
-		if (low_duration == 3 && high_duration == 1)
-		{
-			// It's a 0
-			value |= (0 << i);
-		}
-		else if (low_duration == 1 && high_duration == 3)
-		{
-			// It's a 1
-			value |= (1 << i);
-		}
-		else if (low_duration == 1 && high_duration == 2)
-		{
-			// It's a STOP bit
-			break;
-		}
-	}
-	return value;
-}
-void N64::sendToSystem(uint8_t data)
-{
-}
-void N64::resetController()
-{
-	// Reset the joystick to 0,0 position
-	N64ControllerState &= ~(JOYSTICK_X_MASK | JOYSTICK_Y_MASK);
-}
-void N64::sendControllerInfo()
-{
-	uint8_t info[3];
+// 		// Check the durations to determine the bit value
+// 		if (low_duration == 3 && high_duration == 1)
+// 		{
+// 			// It's a 0
+// 			value |= (0 << i);
+// 		}
+// 		else if (low_duration == 1 && high_duration == 3)
+// 		{
+// 			// It's a 1
+// 			value |= (1 << i);
+// 		}
+// 		else if (low_duration == 1 && high_duration == 2)
+// 		{
+// 			// It's a STOP bit
+// 			break;
+// 		}
+// 	}
+// 	return value;
+// }
+// void N64::sendToSystem(uint8_t data)
+// {
+// }
+// void N64::resetController()
+// {
+// 	// Reset the joystick to 0,0 position
+// 	N64ControllerState &= ~(JOYSTICK_X_MASK | JOYSTICK_Y_MASK);
+// }
+// void N64::sendControllerInfo()
+// {
+// 	uint8_t info[3];
 
-	// First two bytes for controller
-	info[0] = 0x05;
-	info[1] = 0x00;
+// 	// First two bytes for controller
+// 	info[0] = 0x05;
+// 	info[1] = 0x00;
 
-	// Third byte for pack installed
-	info[2] = 0x01;
+// 	// Third byte for pack installed
+// 	info[2] = 0x01;
 
-	// Send the info to the system
-	for (int i = 0; i < 3; i++)
-	{
-		sendToSystem(info[i]);
-	}
-}
-void N64::sendControllerState(){
+// 	// Send the info to the system
+// 	for (int i = 0; i < 3; i++)
+// 	{
+// 		sendToSystem(info[i]);
+// 	}
+// }
+// void N64::sendControllerState(){
 	
-}
+// }
--- src/headers/N64.h
+++ src/headers/N64.h
@@ -1,59 +1,59 @@
-#ifndef N64_H
-#define N64_H
+// #ifndef N64_H
+// #define N64_H
 
-#include "IController.h"
-const uint16_t N64_BUTTON_A = 0x8000;
-const uint16_t N64_BUTTON_B = 0x4000;
-const uint16_t N64_BUTTON_Z = 0x2000;
-const uint16_t N64_BUTTON_START = 0x1000;
-const uint16_t N64_BUTTON_DPAD_UP = 0x0800;
-const uint16_t N64_BUTTON_DPAD_DOWN = 0x0400;
-const uint16_t N64_BUTTON_DPAD_LEFT = 0x0200;
-const uint16_t N64_BUTTON_DPAD_RIGHT = 0x0100;
-const uint16_t N64_BUTTON_RESERVED = 0x0080; // Reserved, always 0
-const uint16_t N64_BUTTON_L = 0x0020;
-const uint16_t N64_BUTTON_R = 0x0010;
-const uint16_t N64_BUTTON_C_UP = 0x0008;
-const uint16_t N64_BUTTON_C_DOWN = 0x0004;
-const uint16_t N64_BUTTON_C_LEFT = 0x0002;
-const uint16_t N64_BUTTON_C_RIGHT = 0x0001;
+// #include "IController.h"
+// const uint16_t N64_BUTTON_A = 0x8000;
+// const uint16_t N64_BUTTON_B = 0x4000;
+// const uint16_t N64_BUTTON_Z = 0x2000;
+// const uint16_t N64_BUTTON_START = 0x1000;
+// const uint16_t N64_BUTTON_DPAD_UP = 0x0800;
+// const uint16_t N64_BUTTON_DPAD_DOWN = 0x0400;
+// const uint16_t N64_BUTTON_DPAD_LEFT = 0x0200;
+// const uint16_t N64_BUTTON_DPAD_RIGHT = 0x0100;
+// const uint16_t N64_BUTTON_RESERVED = 0x0080; // Reserved, always 0
+// const uint16_t N64_BUTTON_L = 0x0020;
+// const uint16_t N64_BUTTON_R = 0x0010;
+// const uint16_t N64_BUTTON_C_UP = 0x0008;
+// const uint16_t N64_BUTTON_C_DOWN = 0x0004;
+// const uint16_t N64_BUTTON_C_LEFT = 0x0002;
+// const uint16_t N64_BUTTON_C_RIGHT = 0x0001;
 
-#define JOYSTICK_X_MASK 0x00FF // Last 8 bits
-#define JOYSTICK_Y_MASK 0xFF00 // Second to last 8 bits
-#define PULSE_LENGTH_THRESHOLD 4
+// #define JOYSTICK_X_MASK 0x00FF // Last 8 bits
+// #define JOYSTICK_Y_MASK 0xFF00 // Second to last 8 bits
+// #define PULSE_LENGTH_THRESHOLD 4
 
-enum N64Command
-{
-	RESETANDINFO = 0xFF,
-	INFO = 0x00,
-	CONTROLLER_STATE = 0x01,
-	READ_CONTROLLER_ACCESSORY = 0x02,
-	WRITE_CONTROLLER_ACCESSORY = 0x03,
-	READ_EEPROM = 0x04,
-	WRITE_EEPROM = 0x05,
-	RTC_INFO = 0x06,
-	READ_RTC_BLOCK = 0x07,
-	WRITE_RTC_BLOCK = 0x08,
-};
+// enum N64Command
+// {
+// 	RESETANDINFO = 0xFF,
+// 	INFO = 0x00,
+// 	CONTROLLER_STATE = 0x01,
+// 	READ_CONTROLLER_ACCESSORY = 0x02,
+// 	WRITE_CONTROLLER_ACCESSORY = 0x03,
+// 	READ_EEPROM = 0x04,
+// 	WRITE_EEPROM = 0x05,
+// 	RTC_INFO = 0x06,
+// 	READ_RTC_BLOCK = 0x07,
+// 	WRITE_RTC_BLOCK = 0x08,
+// };
 
-class N64
-{
-public:
-	N64(int dataPin);
+// class N64
+// {
+// public:
+// 	N64(int dataPin);
 
-private:
-	int dataPin;
-	uint16_t N64ControllerState;
-	uint16_t translateToFormat(GamepadState data);
-	uint32_t recieveFromSystem();
-	int scaleJoystick(int gamepadValue);
-	uint8_t reverseBits(uint8_t num);
-	void listenForCommands();
-	void resetController();
-	void sendControllerInfo();
-	void sendControllerState();
-	void sendToSystem(uint8_t data);
-	uint8_t N64::readByte()
-};
+// private:
+// 	int dataPin;
+// 	uint16_t N64ControllerState;
+// 	uint16_t translateToFormat(GamepadState data);
+// 	uint32_t recieveFromSystem();
+// 	int scaleJoystick(int gamepadValue);
+// 	uint8_t reverseBits(uint8_t num);
+// 	void listenForCommands();
+// 	void resetController();
+// 	void sendControllerInfo();
+// 	void sendControllerState();
+// 	void sendToSystem(uint8_t data);
+// 	uint8_t N64::readByte()
+// };
 
-#endif // N64_H
+// #endif // N64_H
